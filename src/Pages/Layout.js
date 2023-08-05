e9b11f784ced9e5cd8fdeb49030eb41746445dc3
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-08-05 23:33:20 +0800

    added pages

---
+++ src/Pages/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Layout() {
+    return (
+        <div className="widt">
+            <h1 className="">
+
+            </h1>
+        </div>
+    )
+}
