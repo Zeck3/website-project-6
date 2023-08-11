5b06f0cc12180f9c98fd6e01e0524da5c48c6e7f
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-08-11 21:12:12 +0800

    updated tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    backgroundImage: {
+      "drop-shadow" : "linear-gradient(0deg, rgba(8,4,1,0.2665441176470589) 62%, rgba(8,4,1,1) 100%);",
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
