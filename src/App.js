d7a315c73cef9b5e262585882f0497d4c97f0c2b
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-08-11 21:13:25 +0800

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Layout from "./Pages/Layout";
-import MainPage from "./Pages/MainPage";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoadingScreen from "./Components/Fallback"
 
-function App() {
+const Yorushika = lazy(() => import("./Routes/Yorushika"))
+const ThoughtCrime = lazy(() => import("./Routes/ThoughtCrime"))
+const Plagiarism = lazy(() => import("./Routes/Plagiarism"))
+const Prostitution = lazy(() => import("./Routes/Prostitution"))
+const TheSettingSun = lazy(() => import("./Routes/TheSettingSun"))
+const ThatsWhyIGaveUpOnMusic = lazy(() => import("./Routes/ThatsWhyIGaveUpOnMusic"))
+
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<MainPage/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Suspense fallback={<LoadingScreen/>}>
+        <Routes>
+          <Route path="/" element={<Yorushika/>}></Route>
+          <Route path="/thoughtCrime" element={<ThoughtCrime/>}></Route>
+          <Route path="/plagiarism" element={<Plagiarism/>}></Route>
+          <Route path="/prostitution" element={<Prostitution/>}></Route>
+          <Route path="/theSettingSun" element={<TheSettingSun/>}></Route>
+          <Route path="/thatsWhyIGaveUpOnMusic" element={<ThatsWhyIGaveUpOnMusic/>}></Route>
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
-
-export default App;
