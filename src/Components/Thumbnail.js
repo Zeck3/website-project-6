7a1fe13861dbdd5ff2de205bee8f02403a25dce0
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-08-14 00:58:15 +0800

    removed unnecessary components

--- src/Components/Thumbnail.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Thumbnail(props) {
-    return (
-        <img className="absolute bottom-0 right-0 flex object-cover w-full h-full -z-20 ease-in" src={props.Source} alt={props.Alt}></img>
-    )
-}
